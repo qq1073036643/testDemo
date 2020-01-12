bf80b5f2f67c864600c6e417de53ef420b6ceed6
xlluo <xlluo@qq.com>
2020-01-12 11:51:49 +0800

    test1里修改了test3

--- src/testDemo/Test1.java
+++ src/testDemo/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test2");
-		System.out.println(111);
+		System.out.println("test3");
 	}
 }
